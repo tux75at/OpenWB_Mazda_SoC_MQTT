fe60c6ba8dba2f67f61c7106d9c0685aad00e95d
tux75at <tux75at@gmail.com>
2023-01-14 22:54:01 +0100

    Temporarily removed logging, error at installation but works also without or defect installation, needs some testing

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ apt upgrade -y
 apt install -y python3-pip libffi-dev python3-dev cargo pkg-config build-essential libssl-dev git
 
 echo "install required python modules"
-pip3 install logging
 pip3 install paho-mqtt asyncio secrets
 
 echo "install rust to compile newest cryptography module which is required for pymazda"
