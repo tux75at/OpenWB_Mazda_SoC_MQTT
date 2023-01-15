4f37f46a509dd2a3039b49291bef7cb01cc340f9
tux75at <tux75at@gmail.com>
2023-01-15 08:53:28 +0100

    Test for script changes

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "install required packages..."
-apt update
-apt upgrade -y
-apt install -y python3-pip libffi-dev python3-dev cargo pkg-config build-essential libssl-dev git
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y python3-pip libffi-dev python3-dev cargo pkg-config build-essential libssl-dev git
 
 echo "install required python modules"
-pip3 install paho-mqtt asyncio secrets
+sudo pip3 install paho-mqtt asyncio secrets
 
 echo "install rust to compile newest cryptography module which is required for pymazda"
 echo "this will take a some time..."
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 
 echo "compile newest cryptography module, this will take some time..."
-pip3 install cryptography
+sudo pip3 install cryptography
 
 echo "installing pymazda"
-pip3 install pymazda
+sudo pip3 install pymazda
 
 echo "Cloning the GIT repository"
-git clone https://github.com/tux75at/OpenWB_Mazda_SoC_MQTT.git
+git clone https://github.com/tux75at/OpenWB_Mazda_SoC_MQTT.git --branch main
